46d3350d32b16dd21e5fec58edf374db0aaf3931
Nicholas Marrone <nickmarrone@gmail.com>
2026-03-14 10:03:07 -0700

    ADVCA VCV Rack plugin with an AD/AR envelope and a VCA with continuously variable response from linear to exponential.

---
+++ ADVCA/src/ADVCA.cpp
@@ -0,0 +1,227 @@
+#include "plugin.hpp"
+
+struct ADVCA : Module {
+	enum ParamId {
+		ATTACK_PARAM,
+		DECAY_PARAM,
+		MODE_PARAM, // 0 = AD, 1 = AR
+		RESPONSE_PARAM, // 0 = Linear, 1 = Exponential
+		CV_ATTEN_PARAM,
+		PARAMS_LEN
+	};
+	enum InputId {
+		GATE_INPUT,
+		IN_INPUT,
+		CV_INPUT,
+		INPUTS_LEN
+	};
+	enum OutputId {
+		ENV_OUTPUT,
+		OUT_OUTPUT,
+		OUTPUTS_LEN
+	};
+	enum LightId {
+		LEVEL_LIGHT,
+		LIGHTS_LEN
+	};
+
+	enum EnvState {
+		ENV_OFF,
+		ENV_ATTACK,
+		ENV_SUSTAIN,
+		ENV_DECAY_RELEASE
+	};
+
+	// Envelope state variables
+	EnvState envState = ENV_OFF;
+	float envOut = 0.f;
+	dsp::SchmittTrigger gateTrigger;
+
+	ADVCA() {
+		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+		
+		configParam(ATTACK_PARAM, 0.f, 1.f, 0.2f, "Attack Time");
+		configParam(DECAY_PARAM, 0.f, 1.f, 0.2f, "Decay/Release Time");
+		configParam(MODE_PARAM, 0.f, 1.f, 0.f, "Mode (0: AD, 1: AR)");
+		configParam(RESPONSE_PARAM, 0.f, 1.f, 0.f, "Response (0: Lin, 1: Exp)");
+		configParam(CV_ATTEN_PARAM, 0.f, 1.f, 1.f, "CV Attenuator");
+		
+		configInput(GATE_INPUT, "Gate");
+		configInput(IN_INPUT, "Audio In");
+		configInput(CV_INPUT, "VCA CV");
+		
+		configOutput(ENV_OUTPUT, "Envelope Out");
+		configOutput(OUT_OUTPUT, "Audio Out");
+	}
+
+	void process(const ProcessArgs& args) override {
+		// --- ENVELOPE ---
+		bool gate = inputs[GATE_INPUT].getVoltage() >= 1.0f;
+		float attackParam = params[ATTACK_PARAM].getValue();
+		float decayParam = params[DECAY_PARAM].getValue();
+		bool isARMode = params[MODE_PARAM].getValue() > 0.5f;
+
+		// Time scaling: Map 0-1 param to 1ms -> 10s using a cubic curve.
+		// A cubic curve (x^3) gives a more natural "log pot" feel than the pure exponential formula,
+		// spreading out the mid-range times more evenly.
+		float baseTime = 0.001f;
+		float maxAttackTime = 2.0f;
+		float maxDecayTime = 10.0f;
+		float attackTime = baseTime + (maxAttackTime - baseTime) * std::pow(attackParam, 3.f);
+		float decayTime = baseTime + (maxDecayTime - baseTime) * std::pow(decayParam, 3.f);
+
+		float attackRate = 1.f / (attackTime * args.sampleRate);
+		float decayRate = 1.f / (decayTime * args.sampleRate);
+
+		if (gateTrigger.process(inputs[GATE_INPUT].getVoltage())) {
+			envState = ENV_ATTACK;
+		}
+
+		switch (envState) {
+			case ENV_OFF:
+				envOut = 0.f;
+				break;
+			case ENV_ATTACK:
+				envOut += attackRate;
+				if (envOut >= 1.f) {
+					envOut = 1.f;
+					envState = isARMode ? ENV_SUSTAIN : ENV_DECAY_RELEASE;
+				} else if (isARMode && !gate) {
+					envState = ENV_DECAY_RELEASE;
+				}
+				break;
+			case ENV_SUSTAIN:
+				envOut = 1.f;
+				if (!gate) {
+					envState = ENV_DECAY_RELEASE;
+				}
+				break;
+			case ENV_DECAY_RELEASE:
+				envOut -= decayRate;
+				if (envOut <= 0.f) {
+					envOut = 0.f;
+					envState = ENV_OFF;
+				}
+				break;
+		}
+
+		// Calculate 0 to 10V Envelope Output
+		outputs[ENV_OUTPUT].setVoltage(envOut * 10.f);
+
+		// --- VCA ---
+		// CV is normalled to ENV_OUTPUT
+		float cvVolts = inputs[CV_INPUT].getNormalVoltage(envOut * 10.f);
+		
+		// Apply attenuator (0 to 1) to CV
+		float cvInput = cvVolts * params[CV_ATTEN_PARAM].getValue();
+		
+		// Map CV (0-10V) to [0, 1] gain scale
+		float gainNorm = clamp(cvInput / 10.f, 0.f, 1.f);
+
+		// Calculate responses
+		float linGain = gainNorm;
+		// Exponential response curve (x^4 mapping from 0 to 1) for a punchy feel,
+		// or standard exp function: we'll use a polynomial approximation for simplicity and punch.
+		float expGain = gainNorm * gainNorm * gainNorm * gainNorm;
+
+		float responseMix = params[RESPONSE_PARAM].getValue(); // 0 = Lin, 1 = Exp
+		
+		// Crossfade linear to exponential
+		float finalGain = crossfade(linGain, expGain, responseMix);
+
+		// Apply VCA
+		float audioIn = inputs[IN_INPUT].getVoltage();
+		float audioOut = audioIn * finalGain;
+		
+		outputs[OUT_OUTPUT].setVoltage(audioOut);
+		
+		// Set LED brightness to the final gain value
+		lights[LEVEL_LIGHT].setBrightness(finalGain);
+	}
+};
+
+struct ADVCALabels : Widget {
+	void draw(const DrawArgs& args) override {
+		std::shared_ptr<Font> font = APP->window->uiFont;
+		if (!font) return;
+		
+		auto drawText = [&](float x, float y, const char* text, float size, NVGcolor color) {
+			nvgFontSize(args.vg, size);
+			nvgFontFaceId(args.vg, font->handle);
+			nvgTextAlign(args.vg, NVG_ALIGN_CENTER | NVG_ALIGN_BASELINE);
+			nvgFillColor(args.vg, color);
+			nvgText(args.vg, x, y, text, NULL);
+		};
+
+		NVGcolor dark = nvgRGB(34, 34, 34);
+		NVGcolor white = nvgRGB(255, 255, 255);
+
+		// Title
+		drawText(30.48f, 25.f, "ADVCA", 10.f, dark);
+		
+		// Switch
+		drawText(30.48f, 36.f, "AR", 8.f, dark);
+		drawText(30.48f, 68.f, "AD", 8.f, dark);
+
+		// Knobs
+		drawText(30.48f, 85.f, "ATTACK", 8.f, dark);
+		drawText(30.48f, 140.f, "DECAY", 8.f, dark);
+
+		// Envelope I/O
+		drawText(16.f, 199.f, "GATE", 8.f, dark);
+		drawText(44.f, 199.f, "ENV", 8.f, white);
+
+		// CV Section
+		drawText(16.f, 281.f, "CV", 8.f, dark);
+		drawText(44.f, 281.f, "AMT", 8.f, dark);
+
+		// VCA I/O
+		drawText(16.f, 324.f, "IN", 8.f, dark);
+		drawText(44.f, 324.f, "OUT", 8.f, white);
+	}
+};
+
+struct ADVCAWidget : ModuleWidget {
+	ADVCAWidget(ADVCA* module) {
+		setModule(module);
+		
+		// 4HP width
+		box.size = Vec(4 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT);
+		
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/ADVCA.svg")));
+
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+		addChild(createWidget<ADVCALabels>(Vec(0, 0)));
+
+		// Measurements
+		float centerX = box.size.x / 2.f;
+		float leftColumn = 16.f;
+		float rightColumn = 44.f;
+
+		// Top Half: Envelope
+		addParam(createParamCentered<CKSS>(Vec(centerX, 48.f), module, ADVCA::MODE_PARAM));
+		
+		addParam(createParamCentered<RoundBlackKnob>(Vec(centerX, 100.f), module, ADVCA::ATTACK_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(Vec(centerX, 155.f), module, ADVCA::DECAY_PARAM));
+		
+		addInput(createInputCentered<PJ301MPort>(Vec(leftColumn, 215.f), module, ADVCA::GATE_INPUT));
+		addOutput(createOutputCentered<PJ301MPort>(Vec(rightColumn, 215.f), module, ADVCA::ENV_OUTPUT));
+
+		// Bottom Half: VCA
+		addParam(createParamCentered<Trimpot>(Vec(centerX, 255.f), module, ADVCA::RESPONSE_PARAM));
+		
+		addChild(createLightCentered<MediumLight<RedLight>>(Vec(centerX, 275.f), module, ADVCA::LEVEL_LIGHT));
+		
+		addInput(createInputCentered<PJ301MPort>(Vec(leftColumn, 295.f), module, ADVCA::CV_INPUT));
+		addParam(createParamCentered<Trimpot>(Vec(rightColumn, 295.f), module, ADVCA::CV_ATTEN_PARAM));
+
+		addInput(createInputCentered<PJ301MPort>(Vec(leftColumn, 340.f), module, ADVCA::IN_INPUT));
+		addOutput(createOutputCentered<PJ301MPort>(Vec(rightColumn, 340.f), module, ADVCA::OUT_OUTPUT));
+	}
+};
+
+Model* modelADVCA = createModel<ADVCA, ADVCAWidget>("ADVCA");
